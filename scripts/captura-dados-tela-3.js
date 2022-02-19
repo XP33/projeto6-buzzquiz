20bf98b2429214309206716f82b326fda4a5eb66
samukaell <snoooydelta37@gmail.com>
2022-02-18 23:00:16 -0300

    Funcao de captura dos dados da tela3 implementada com SUCESSOgit add .git add . XD

--- scripts/captura-dados-tela-3.js
+++ scripts/captura-dados-tela-3.js
@@ -4,4 +4,38 @@ let infoBasico = {
     url: null,
     qtdPerguntas: null,
     qtdNiveis: null
-}
+};
+
+//Pagina 2
+let infoPerguntas = {
+    titulo: null,
+    cor: null,
+    respostaIncorreta1: null,
+    urlImagem1: null,
+    respostaIncorreta2: null,
+    urlImagem2: null,
+    respostaIncorreta3: null,
+    urlImagem3: null
+};
+
+//Pagina 3 
+let infoNiveis = {
+    Nivel1 : {
+        titulo: null,
+        acertosMinimos: null,
+        url: null,
+        descrisao: null
+    },
+    Nivel2 : {
+        titulo: null,
+        acertosMinimos: null,
+        url: null,
+        descrisao: null
+    },
+    Nivel3 : {
+        titulo: null,
+        acertosMinimos: null,
+        url: null,
+        descrisao: null
+    }
+};
