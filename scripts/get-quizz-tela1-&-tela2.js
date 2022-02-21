24a0c95ebe8d85ac1fa8270f2a8eeda155dda469
Pimenta <mateuspimartins@hotmail.com>
2022-02-21 16:52:49 -0300

    Vers Final com tags

--- scripts/get-quizz-tela1-&-tela2.js
+++ scripts/get-quizz-tela1-&-tela2.js
@@ -57,17 +57,17 @@ function printAllQuizzes(response) {
 
             userQuizzesEl2.innerHTML += `<article class="first_quizz hidden">
     <h1>Você não criou nenhum quizz ainda :(</h1>
-    <button onclick="habilitarCriacaoQuizz()">
+    <button onclick="habilitarCriacaoQuizz()" data-identifier="create-quizz">
       <h2>Criar Quizz</h2>
     </button>
   </article>
   
   <section onclick="habilitarCriacaoQuizz()" class="you_quizzes hidden">
         <h1 class="titulo_grupo">Seus Quizzes</h1>
-        <img onclick="habilitarCriacaoQuizz()" src="/media/add_button.png" alt="Criar um novo Quizz">
+        <img onclick="habilitarCriacaoQuizz()" src="/media/add_button.png" data-identifier="create-quizz" alt="Criar um novo Quizz">
 </section><div class="fatherM"><div class=mediaQ>`
 
-            let userQuizTemplate = `<article class="quizz_box quiz" id="${quiz.id}" onclick="openQuiz(this)">
+            let userQuizTemplate = `<article class="quizz_box quiz" id="${quiz.id}" onclick="openQuiz(this)" data-identifier="user-quizzes" data-identifier="quizz-card">
             <img src="${quiz.image}" alt="Quizz image" class="image_quizz"><h1 class="titulo_quizz">${quiz.title}</h1></article>`
 
             userQuizzesEl2.innerHTML += userQuizTemplate
@@ -79,7 +79,7 @@ function printAllQuizzes(response) {
             let title = quiz.title;
             let image = quiz.image;
 
-            let quizTemplate = `<article class="quizz_box quiz" id="${id}" onclick="openQuiz(this)"><img src="${image}" alt="Quizz image" class="image_quizz"><h1 class="titulo_quizz">${title}</h1></article>`
+            let quizTemplate = `<article class="quizz_box quiz" id="${id}" onclick="openQuiz(this)" data-identifier="general-quizzes" data-identifier="quizz-card"><img src="${image}" alt="Quizz image" class="image_quizz"><h1 class="titulo_quizz">${title}</h1></article>`
 
             allQuizzesEl.innerHTML += quizTemplate   
     });
@@ -118,11 +118,11 @@ function displayQuiz(response) {
 
         answers.forEach(answer => {
             templateQuizAnswers += `<div class="option" onclick="clickCardAnswer(this)" data-isCorrectAnswer="${answer.isCorrectAnswer}">
-            <img class="img_option" src=${answer.image} alt=""><p class="name_option">${answer.text}</p></div>`
+            <img class="img_option" src=${answer.image} data-identifier="answer" alt=""><p class="name_option">${answer.text}</p></div>`
         })
 
         templateQuizQuestions += `<nav class="full_question_box" id="${questions.indexOf(question)}">
-        <div class="pergunta_box"><h1>${question.title}</h1></div><div class="options_box">${templateQuizAnswers}</div></nav>`
+        <div class="pergunta_box"><h1>${question.title} data-identifier="question" </h1></div><div class="options_box">${templateQuizAnswers}</div></nav>`
 
         templateQuizAnswers = ""
     })
@@ -179,7 +179,7 @@ function showResult(correctAnswerPercentage) {
     levels.forEach(level => {
         if (correctAnswerPercentage >= level.minValue) {
             templateResult = `
-            <div class="percentage_box"><h1>${correctAnswerPercentage}% de acerto: ${level.title}</h1></div>
+            <div class="percentage_box"><h1>${correctAnswerPercentage}% de acerto: ${level.title} data-identifier="quizz-result"</h1></div>
             <img src=${level.image} alt=""><h2>${level.text}</h2>
             </nav><nav><button class="reiniciar_quizz"><h1>Reiniciar Quizz</h1></button>
             <div class="voltar_home" onclick="returnHome()"><h1>Voltar pra home </h1></div>`
